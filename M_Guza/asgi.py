e3e75752fc1d692183f38ff56ca047dd32d48432
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-25 23:27:42 +0300

    boilerplate

---
+++ M_Guza/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for M_Guza project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'M_Guza.settings')
+
+application = get_asgi_application()
